dac874520ede477e56b79ab7562ca4377bdeda1e
Harry Maynard <maynard321@gmail.com>
2023-07-28 23:51:35 -0700

    Fixed overflow on demo page.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,45 @@
 <script lang="ts" setup>
 import ChatApp from '@/components/ChatApp.vue'
+
+const currentYear = new Date().getFullYear()
 </script>
 
 <template>
-  <div class="component">
+  <div class="page">
     <div class="content">
       <h1>Title Here</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et sapien urna. Suspendisse euismod, elit sed imperdiet vehicula, lectus velit condimentum tortor, nec fringilla nisl metus id risus. Donec lobortis porta enim, eu malesuada metus cursus ut. Morbi tincidunt est nisl. Suspendisse vel nunc eget mi pellentesque tincidunt quis in massa. Aliquam ultrices erat ut dapibus sagittis. Mauris suscipit sagittis orci, at rhoncus enim mollis at. Pellentesque condimentum, magna sit amet ultricies consequat, nibh mi consectetur eros, ac aliquet mi libero eu orci. Aenean viverra nisl vel orci euismod, eu ultrices sem ultricies. Curabitur sollicitudin, dolor id pretium dictum, tortor neque fermentum metus, in pellentesque lacus massa sed lacus. In a neque ac nisl porttitor tempus. Morbi placerat dui vitae mauris consequat commodo. Sed pretium dolor ac nunc sollicitudin, et elementum dui tristique. Duis quis justo id velit dapibus dictum vitae non urna. Maecenas vulputate nec justo id tincidunt.</p>
+      <p>Morbi egestas tellus purus, in dapibus erat fermentum ut. Donec eget convallis justo. In condimentum augue tempus, interdum odio non, euismod dui. Nunc eleifend vitae quam in elementum. Mauris elit eros, dictum et nisl in, tincidunt feugiat quam. Sed sit amet dui ultrices, mattis quam vel, molestie erat. Cras efficitur neque lorem, sed posuere libero placerat nec. Integer scelerisque erat augue, a porta elit pellentesque id. Cras et enim at turpis dictum maximus vulputate ac quam. Suspendisse vulputate, nisl id gravida ultrices, diam dolor pellentesque sapien, id tristique sem arcu nec elit. Cras scelerisque metus quam, ut pharetra libero imperdiet ac. Fusce dolor magna, dignissim ut massa non, sagittis rutrum augue.</p>
+      <p>Aliquam non ex at nisi rutrum placerat. Donec commodo, libero posuere venenatis auctor, nisl lectus gravida quam, non scelerisque nisi arcu sagittis libero. Ut pellentesque placerat massa. Morbi ultrices ante vitae ipsum porttitor ultricies. Curabitur tempor nibh ut metus cursus imperdiet. Nunc semper malesuada mauris sit amet volutpat. Curabitur at dolor fringilla, mollis magna in, fringilla ex. Maecenas quis magna ac nisi gravida feugiat id ac massa. Integer rutrum venenatis euismod. Nullam tortor mi, ultrices ac sapien hendrerit, scelerisque scelerisque elit. Aenean sed augue finibus, gravida nibh id, sodales augue. Aliquam erat volutpat. Cras tellus erat, lobortis a semper nec, suscipit vel urna. Phasellus id posuere ex, eget ultrices metus. Integer fermentum ultrices ligula eu vehicula.</p>
+      <p>Morbi lectus urna, rhoncus in dapibus sit amet, mollis ut nunc. Donec sapien magna, placerat eget elit at, facilisis sagittis tortor. Pellentesque nec eros gravida, accumsan arcu a, mattis lorem. Morbi dapibus nibh nisi, nec gravida lorem luctus at. Mauris mi ipsum, viverra quis ligula at, efficitur tempor tellus. In hac habitasse platea dictumst. Vivamus non dui mi. Integer molestie sed dui at imperdiet. Fusce sed tortor massa. Integer ut viverra nunc, vel dictum magna.</p>
+      <p>Nullam dolor sapien, dapibus id eleifend sed, consequat at dui. Maecenas in tempus ipsum, ut eleifend est. Nam ut laoreet metus. Nunc nec ligula sit amet nisl pellentesque fringilla. Maecenas vitae euismod turpis, non posuere urna. Fusce nisi dui, tincidunt ut diam at, sollicitudin mollis nisl. Etiam bibendum, arcu et ultrices placerat, urna mi luctus nisl, ac mattis massa eros id tellus. Phasellus ac molestie dui. Suspendisse non laoreet mauris. Duis semper scelerisque rhoncus. Nam accumsan eu leo dapibus commodo. Nullam vel sapien massa. Fusce sit amet augue varius, feugiat elit ut, porttitor nulla.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et sapien urna. Suspendisse euismod, elit sed imperdiet vehicula, lectus velit condimentum tortor, nec fringilla nisl metus id risus. Donec lobortis porta enim, eu malesuada metus cursus ut. Morbi tincidunt est nisl. Suspendisse vel nunc eget mi pellentesque tincidunt quis in massa. Aliquam ultrices erat ut dapibus sagittis. Mauris suscipit sagittis orci, at rhoncus enim mollis at. Pellentesque condimentum, magna sit amet ultricies consequat, nibh mi consectetur eros, ac aliquet mi libero eu orci. Aenean viverra nisl vel orci euismod, eu ultrices sem ultricies. Curabitur sollicitudin, dolor id pretium dictum, tortor neque fermentum metus, in pellentesque lacus massa sed lacus. In a neque ac nisl porttitor tempus. Morbi placerat dui vitae mauris consequat commodo. Sed pretium dolor ac nunc sollicitudin, et elementum dui tristique. Duis quis justo id velit dapibus dictum vitae non urna. Maecenas vulputate nec justo id tincidunt.</p>
+      <p>Morbi egestas tellus purus, in dapibus erat fermentum ut. Donec eget convallis justo. In condimentum augue tempus, interdum odio non, euismod dui. Nunc eleifend vitae quam in elementum. Mauris elit eros, dictum et nisl in, tincidunt feugiat quam. Sed sit amet dui ultrices, mattis quam vel, molestie erat. Cras efficitur neque lorem, sed posuere libero placerat nec. Integer scelerisque erat augue, a porta elit pellentesque id. Cras et enim at turpis dictum maximus vulputate ac quam. Suspendisse vulputate, nisl id gravida ultrices, diam dolor pellentesque sapien, id tristique sem arcu nec elit. Cras scelerisque metus quam, ut pharetra libero imperdiet ac. Fusce dolor magna, dignissim ut massa non, sagittis rutrum augue.</p>
+      <p>Aliquam non ex at nisi rutrum placerat. Donec commodo, libero posuere venenatis auctor, nisl lectus gravida quam, non scelerisque nisi arcu sagittis libero. Ut pellentesque placerat massa. Morbi ultrices ante vitae ipsum porttitor ultricies. Curabitur tempor nibh ut metus cursus imperdiet. Nunc semper malesuada mauris sit amet volutpat. Curabitur at dolor fringilla, mollis magna in, fringilla ex. Maecenas quis magna ac nisi gravida feugiat id ac massa. Integer rutrum venenatis euismod. Nullam tortor mi, ultrices ac sapien hendrerit, scelerisque scelerisque elit. Aenean sed augue finibus, gravida nibh id, sodales augue. Aliquam erat volutpat. Cras tellus erat, lobortis a semper nec, suscipit vel urna. Phasellus id posuere ex, eget ultrices metus. Integer fermentum ultrices ligula eu vehicula.</p>
+      <p>Morbi lectus urna, rhoncus in dapibus sit amet, mollis ut nunc. Donec sapien magna, placerat eget elit at, facilisis sagittis tortor. Pellentesque nec eros gravida, accumsan arcu a, mattis lorem. Morbi dapibus nibh nisi, nec gravida lorem luctus at. Mauris mi ipsum, viverra quis ligula at, efficitur tempor tellus. In hac habitasse platea dictumst. Vivamus non dui mi. Integer molestie sed dui at imperdiet. Fusce sed tortor massa. Integer ut viverra nunc, vel dictum magna.</p>
+      <p>Nullam dolor sapien, dapibus id eleifend sed, consequat at dui. Maecenas in tempus ipsum, ut eleifend est. Nam ut laoreet metus. Nunc nec ligula sit amet nisl pellentesque fringilla. Maecenas vitae euismod turpis, non posuere urna. Fusce nisi dui, tincidunt ut diam at, sollicitudin mollis nisl. Etiam bibendum, arcu et ultrices placerat, urna mi luctus nisl, ac mattis massa eros id tellus. Phasellus ac molestie dui. Suspendisse non laoreet mauris. Duis semper scelerisque rhoncus. Nam accumsan eu leo dapibus commodo. Nullam vel sapien massa. Fusce sit amet augue varius, feugiat elit ut, porttitor nulla.</p>
     </div>
+    <footer>
+      &copy; {{ currentYear }} Harry Maynard
+    </footer>
   </div>
   <ChatApp />
 </template>
 
 <style lang="scss" scoped>
-.component {
-  width: 100%;
-  height: 100%;
+.page {
+  max-width: 768px;
+  height: inherit;
+  margin: auto;
 
   .content {
-    max-width: 768px;
-    height: inherit;
-    margin: auto;
     padding: 20px;
     border: solid thin #CCC;
     background: #EEE;
+  }
+  footer {
+    padding: 20px;
+    color: #888;
   }
 }
 </style>
