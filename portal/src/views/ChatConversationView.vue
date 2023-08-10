fe5c5d11f21cb0c36cc1981010368d5483363232
Harry Maynard <maynard321@gmail.com>
2023-08-10 00:20:19 -0700

    Added form fields for sending messages in portal.

--- portal/src/views/ChatConversationView.vue
+++ portal/src/views/ChatConversationView.vue
@@ -1,5 +1,10 @@
 <script lang="ts" setup>
 import SideBar from '@/components/SideBar.vue'
+import ChatConversation from 'common/components/ChatConversation.vue'
+
+const handleSubmitSendMessage = () => {
+  console.log('send message')
+}
 </script>
 
 <template>
@@ -7,7 +12,25 @@ import SideBar from '@/components/SideBar.vue'
     <div class="sidebar border-end">
       <SideBar/>
     </div>
-    <div class="flex-fill"></div>
+    <div class="flex-fill d-flex flex-column">
+      <div class="flex-fill position-relative">
+        <ChatConversation/>
+      </div>
+      <div class="m-3">
+        <form
+          class="d-flex"
+          @submit.prevent="handleSubmitSendMessage"
+        >
+          <input
+            class="form-control flex-fill"
+          />
+          <button
+            type="submit"
+            class="btn btn-primary ms-3"
+          >Send</button>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
