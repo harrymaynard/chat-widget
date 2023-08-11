6ffda9f30d6b7650627a9fe3febd36abdba20ed1
Harry Maynard <maynard321@gmail.com>
2023-08-10 21:10:00 -0700

    Setup Redis connection.

--- backend/src/services/WebSocketService.ts
+++ backend/src/services/WebSocketService.ts
@@ -1,10 +1,19 @@
 import { Server, type Socket } from 'socket.io'
+import { createAdapter } from '@socket.io/redis-adapter'
+import { Emitter } from '@socket.io/redis-emitter'
+import { createClient } from 'redis'
 import { formatISODateTime } from 'common/helpers/DateHelper'
 import LogService from './LogService'
 import IMessage from 'common/interfaces/IMessage'
 
+interface WebSocketEventTypes {
+  message: (message: IMessage) => void
+  time: (date: Date) => void
+}
+
 export default class WebSocketService {
   private io: Server | null = null
+  private emitter: Emitter | null = null
 
   constructor() {}
 
@@ -12,6 +21,8 @@ export default class WebSocketService {
     this.io = new Server(httpServer, {
       path: '/api/socket',
     })
+
+    await this.initRedisConnection()
 
     this.io.on('connection', (socket: Socket) => {
       LogService.info('User connected')
@@ -44,5 +55,30 @@ export default class WebSocketService {
         }
       })
     })
+  }
+
+  private async initRedisConnection() {
+    const pubClient = createClient({
+      url: 'redis://user:pass@localhost:6379'
+    })
+    const subClient = pubClient.duplicate()
+
+    try {
+      await Promise.all([
+        pubClient.connect(),
+        subClient.connect()
+      ]).then(() => {
+        // Create and setup Redis adapter.
+        this.io?.adapter(createAdapter(pubClient, subClient))
+        
+        // Setup emitter.
+        this.emitter = new Emitter<WebSocketEventTypes>(pubClient)
+        setInterval(() => {
+          this.emitter?.emit('time', new Date())
+        }, 5000)
+      })
+    } catch (error) {
+      LogService.error('Failed to connect to Redis server')
+    }
   }
 }
