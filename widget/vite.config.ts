a5e235fc6b61db2d3672ea208e7dfdcd7be3b95d
Harry Maynard <maynard321@gmail.com>
2023-08-22 23:26:02 -0700

    Split app from vendor assets during build.

--- widget/vite.config.ts
+++ widget/vite.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import eslintPlugin from 'vite-plugin-eslint'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), eslintPlugin()],
+  plugins: [
+    vue(),
+    splitVendorChunkPlugin(),
+    eslintPlugin()
+  ],
   resolve: {
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
     alias: {
